a9b1caf30ce667d437541d9ca156367b5987262c
Florian Catinaud <catinaud.florian@gmail.com>
2022-04-21 00:32:23 +0200

    Project soon done. Need to config php-fpm on wordpress and check mysql connection

--- Makefile
+++ Makefile
@@ -8,8 +8,12 @@ endif
 
 # $(DOCKER) -f srcs/docker-compose.yaml build
 all		:
+	mkdir -p /Users/florian/data/wordpress
+	mkdir -p /Users/florian/data/db_data
 	$(DOCKER) -f srcs/docker-compose.yml build
 	$(DOCKER) -f srcs/docker-compose.yml up
 
+stop	:
+	$(DOCKER) -f srcs/docker-compose.yml stop
 fclean	:
 	docker system prune -f
--- srcs/requirements/wordpress/config/script.sh
+++ srcs/requirements/wordpress/config/script.sh
@@ -1,8 +1,16 @@
-mkdir /var/www
-mkdir /var/www/html
-cd /var/www/html
-wget http://wordpress.org/latest.tar.gz
-tar xfz latest.tar.gz
-rm -rf latest.tar.gz
-mv wordpress/* ./
-rm -r wordpress
+
+wp-cli  core download --allow-root
+
+rm -f /var/www/html/wp-config.php
+cp /tmp/wp-config.php /var/www/html/.
+
+cat /var/script.sh
+
+chown -R www-data:www-data /var/www/html/
+
+wp-cli core install --url="fcatinau.42.fr" --title="My website" --admin_user="fcatinau" \
+    --admin_password="fcatinau_42" --admin_email="fcatinau@student.42.fr" --skip-email --allow-root
+wp-cli config create --dbname="wordpress" --dbuser="wordpress" --dbpass="2134tgefw456y*" --allow-root 
+wp-cli user create fcatinau user@fcatinau.42.fr --role=author  --allow-root
+
+exec php-fpm7.3 -F -R
