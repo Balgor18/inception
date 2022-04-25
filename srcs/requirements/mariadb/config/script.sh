f6dfd0452f6033ecdbc44b47f7f5f94e7005bdf3
Florian Catinaud <catinaud.florian@gmail.com>
2022-04-25 23:10:56 +0200

    Little modif

--- srcs/requirements/mariadb/config/script.sh
+++ srcs/requirements/mariadb/config/script.sh
@@ -1,23 +1,24 @@
-# cat << EOF > /var/conf.log
-# CREATE DATABASE $DB_NAME;
+# cat << EOF > info.sql
+# CREATE DATABASE IF NOT EXISTS $DB_NAME;
+# SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$PASSWORD');
 # GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION;
-# SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$PASSWORD') ;
-# GRANT ALL ON *.* TO '$ADMIN_USER'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';
-
-# GRANT ALL ON $DB_NAME.* TO '$USER'@'%' IDENTIFIED BY '$WP_PASSWORD';
+# CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED by '$USER_PASSWORD';
+# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER'@'%' WITH GRANT OPTION;
 # FLUSH PRIVILEGES;
-# exit
 # EOF
 
-cat << EOF > info.sql
+cat << EOF > /var/info.sql
 CREATE DATABASE IF NOT EXISTS wordpress;
 SET PASSWORD FOR 'root'@'localhost'=PASSWORD('1R00tP4sword1*');
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '1R00tP4sword1*' WITH GRANT OPTION;
-CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED by '2134tgefw456y*s';
+CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED by '2134tgefw456y*';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 
-service mysql start && mysql < info.sql
+mysqld --user=mysql --skip-name-resolve --bootstrap --skip-networking=0 < /var/info.sql
+
+service mysql start
+#&& mysql < info.sql
 
 # rm /var/conf.log
