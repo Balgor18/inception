6293c0aaed7ed4255be33f0bc0a983c0a03d8e26
Florian Catinaud <catinaud.florian@gmail.com>
2022-04-26 21:23:37 +0200

    try something

--- srcs/requirements/mariadb/config/script.sh
+++ srcs/requirements/mariadb/config/script.sh
@@ -1,13 +1,13 @@
-# cat << EOF > /var/conf.log
-# CREATE DATABASE $DB_NAME;
-# GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION;
-# SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$PASSWORD') ;
-# GRANT ALL ON *.* TO '$ADMIN_USER'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';
+cat << EOF > /var/conf.log
+CREATE DATABASE $DB_NAME;
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$PASSWORD' WITH GRANT OPTION;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('$PASSWORD') ;
+GRANT ALL ON *.* TO '$ADMIN_USER'@'%' IDENTIFIED BY '$ADMIN_PASSWORD';
 
-# GRANT ALL ON $DB_NAME.* TO '$USER'@'%' IDENTIFIED BY '$WP_PASSWORD';
-# FLUSH PRIVILEGES;
-# exit
-# EOF
+GRANT ALL ON $DB_NAME.* TO '$USER'@'%' IDENTIFIED BY '$WP_PASSWORD';
+FLUSH PRIVILEGES;
+exit
+EOF
 
 cat << EOF > info.sql
 CREATE DATABASE IF NOT EXISTS wordpress;
@@ -18,6 +18,6 @@ GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
 
-service mysql start && mysql < info.sql
+service mysql start && mysql "-p1R00tP4sword1*" < info.sql
 
 # rm /var/conf.log
