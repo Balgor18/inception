8a2bf97bcae35a66ed68efa389e1fcd33656505d
Florian Catinaud <catinaud.florian@gmail.com>
2022-04-26 18:48:47 +0200

    We try something

--- srcs/requirements/mariadb/config/script.sh
+++ srcs/requirements/mariadb/config/script.sh
@@ -7,17 +7,17 @@
 # FLUSH PRIVILEGES;
 # EOF
 
-cat << EOF > /var/info.sql
-CREATE DATABASE IF NOT EXISTS wordpress;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('1R00tP4sword1*');
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '1R00tP4sword1*' WITH GRANT OPTION;
-CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED by '2134tgefw456y*';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
+# cat << EOF > /var/info.sql
+# CREATE DATABASE IF NOT EXISTS wordpress;
+# SET PASSWORD FOR 'root'@'localhost'=PASSWORD('1R00tP4sword1*');
+# GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '1R00tP4sword1*' WITH GRANT OPTION;
+# CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED by '2134tgefw456y*';
+# GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%' WITH GRANT OPTION;
+# FLUSH PRIVILEGES;
+# EOF
 
-mysqld --user=mysql --skip-name-resolve --bootstrap --skip-networking=0 < /var/info.sql
-
+# mysqld --user=mysql --skip-name-resolve --bootstrap --skip-networking=0 < /var/info.sql
+mysql -h "localhost" -u "root" "-p1R00tP4sword1*" "wordpress" < /var/info.sql
 service mysql start
 #&& mysql < info.sql
 
