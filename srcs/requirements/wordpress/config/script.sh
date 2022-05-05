a0cf1751e7a6c9bcd750b9fa2df3e66e3e9a9f47
Florian CATINAUD <fcatinau@e2r6p16.clusters.42paris.fr>
2022-05-05 12:10:05 +0200

    Some little modif

--- srcs/requirements/wordpress/config/script.sh
+++ srcs/requirements/wordpress/config/script.sh
@@ -5,11 +5,9 @@ if  [ ! -f "$FILE" ]; then
 	echo "Create config"
 	rm -rf /var/www/html/wp-config.php
 	wp config create --dbname=$DB_NAME --dbuser="wordpress" --dbpass=$WP_PASSWORD --dbhost="mariadb" --path="/var/www/html/" --allow-root --skip-check
-	# wp core install --url="fcatinau.42.fr" --title="fcatinau site" --admin_user="fcatinau" --admin_password="fcatinau_42" --admin_email="fcatinau@student.42.fr" --path="/var/www/html/" --allow-root
 	wp core install --url="localhost" --title="fcatinau site" --admin_user="fcatinau" --admin_password="fcatinau_42" --admin_email="fcatinau@student.42.fr" --path="/var/www/html/" --allow-root
 	wp user create ajeanne ajeanne@student.42.fr --role=author --user_pass="qwerty12" --allow-root
 	touch /var/www/html/.exist
 fi
 echo "Config already done"
 exec php-fpm7.3 --nodaemonize
-# tail -f /dev/null
